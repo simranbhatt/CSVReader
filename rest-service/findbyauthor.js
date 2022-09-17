a8a712839c8496d441362a3e3c18e19821424e22
Simran <simrankbhatt@gmail.com>
2022-09-17 11:55:38 +0530

    minor code improvements

--- rest-service/findbyauthor.js
+++ rest-service/findbyauthor.js
@@ -2,12 +2,8 @@ const fileStream = require('fs')
 const multiStream = require('multistream')
 var removeBOM = require('remove-bom-stream');
 const csv = require("csv-parser")
-const express = require('express')
-const app = express()
-require('dotenv').config({path: '../.env'})
 
-//email: email address of the author
-//app.get('/findByAuthor/:email',
+//function to find all books/magazines that match the author email passed in GET params
  exports.author = function(req, res) {
     var dataArray = []
     const email = req.params.email;
@@ -19,9 +15,12 @@ require('dotenv').config({path: '../.env'})
     new multiStream(streams)
     .pipe(removeBOM('utf-8'))
     .pipe(csv({separator: ';'}))
+    //adding books/magazines by author to an array
     .on("data", (row) => { 
-        if(row.authors == email) dataArray.push(row)
+        if(row.authors == email) 
+            dataArray.push(row)
     })
+    //returning final array of matched books/magazines
     .on("end", () => res.json(dataArray))
     .on("error", (error) => console.log(error.message))
 };
